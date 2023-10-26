77f9a2311b71d43c7efb2dc3229fa385618bc64d
Egor Yurchenko <admegor@mail.ru>
2023-10-26 19:40:03 +0500

    private-person-main

--- src/js/module/portfolio.js
+++ src/js/module/portfolio.js
@@ -1,24 +1,7 @@
 let $sliderPortfolio = $('.portfolio-main-list');
 
-// if ($(window).width() < 768) {
-//     console.log('активировать слайдер1');
-//     $sliderPortfolio.not('.slick-initialized').slick({
-//         dots: false,
-//         infinite: false,
-//         speed: 100,
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         arrows: false,
-//         variableWidth: true
-//     });
-// } else {
-//     $sliderPortfolio.filter('.slick-initialized').slick('unslick');
-//     console.log('убрать слайдер1');
-// }
-
 $(window).on('load resize', function() {
     if ($(window).width() < 768) {
-        console.log('активировать слайдер2');
         $sliderPortfolio.not('.slick-initialized').slick({
             dots: false,
             infinite: false,
@@ -29,7 +12,6 @@ $(window).on('load resize', function() {
             variableWidth: true
         });
     } else {
-        console.log('убрать слайдер2');
         $sliderPortfolio.filter('.slick-initialized').slick('unslick');
     }
 });
